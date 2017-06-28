c5779bc7a029763e53c195e1f0bf677d1565184d
Arran France <arranf@gmail.com>
2017-06-28 19:35:27 +0100

    Add GitService interface

--- API/Github.cs
+++ API/Github.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using com.bricksandmortarstudio.GitEmailTemplateSync.Model;
 using RestSharp;
 
 namespace com.bricksandmortarstudio.GitEmailTemplateSync
 {
-    public class Api
+    public class Github : GitService
     {
         RestClient _client = new RestClient( "https://api.github.com" );
 
-        
+
+        public IEnumerable<GitFile> UpdateGitFiles()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
