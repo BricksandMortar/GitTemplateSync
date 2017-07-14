7778d72adc0c1b314f98ce5e8f1d4bf398517179
Arran France <arranf@gmail.com>
2017-07-14 13:22:47 +0100

    Turn Github API into IGitService

--- API/Github.cs
+++ API/Github.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using com.bricksandmortarstudio.GitEmailTemplateSync.API;
 using com.bricksandmortarstudio.GitEmailTemplateSync.Model;
 using RestSharp;
 
 namespace com.bricksandmortarstudio.GitEmailTemplateSync
 {
-    public class Github : GitService
+    public class Github : IGitService
     {
         RestClient _client = new RestClient( "https://api.github.com" );
 
